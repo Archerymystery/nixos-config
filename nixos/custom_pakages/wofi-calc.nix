aab4220b1d9e8c75b3c1c1e4520603b75a78b3a4
Archerymystery <ratibor200805@gmail.com>
2026-01-20 17:08:55 +0200

    createt custom pkg wofi-calc

---
+++ nixos/custom_pakages/wofi-calc.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.stdenv.mkDerivation rec {
+  pname = "wofi-calc";
+  version = "1.1";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "Zeioth";
+    repo = "wofi-calc";
+    rev = "edd316f3f40a6fcb2afadf5b6d9b14cc75a901e0";
+    hash = "sha256-y8GoTHm0zPkeXhYS/enNAIrU+RhrUMnQ41MdHWWTPas=";
+  };
+  buildInputs = [ pkgs.libqalculate pkgs.wofi ];
+  installPhase = ''
+    mkdir -p $out/bin
+    cp wofi-calc.sh $out/bin/${pname}
+    chmod +x $out/bin/${pname}
+  '';
+}
