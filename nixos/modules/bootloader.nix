06bb937a93f393d600aeb836f247a6855c1e9ceb
Archerymystery <ratibor200805@gmail.com>
2026-01-31 01:36:31 +0200

    remove comments

--- nixos/modules/bootloader.nix
+++ nixos/modules/bootloader.nix
@@ -5,8 +5,7 @@
   boot.loader.grub.extraEntries = ''
     menuentry "Bazzite Linux" { 
       search --set=root --fs-uuid 2c628e7d-a3c8-4918-a236-91db2e57db86 
-      # Це типовий шлях для Fedora/Bazzite EFI файлу. Може знадобитися коригування.
-      chainloader /EFI/fedora/grubx64.efi
+       chainloader /EFI/fedora/grubx64.efi
     }
   '';
 }
