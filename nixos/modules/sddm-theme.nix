d09561e1e31745f12de5acc63d67bd513070934b
Archerymystery <ratibor200805@gmail.com>
2026-01-31 01:29:42 +0200

    remove comments

--- nixos/modules/sddm-theme.nix
+++ nixos/modules/sddm-theme.nix
@@ -2,18 +2,14 @@
 
 pkgs.stdenv.mkDerivation {
   pname = "sddm-astronaut-theme";
-  version = "1.0"; # Можна вказати версію або дату
+  version = "1.0";
 
   src = pkgs.fetchFromGitHub {
     owner = "Keyitdev";
     repo = "sddm-astronaut-theme";
-    rev = "d73842c761f7d7859f3bdd80e4360f09180fad41"; # Рекомендується замінити на конкретний commit hash для стабільності
-    sha256 = "1lvbvs58w1jx2y490vb4vpwqs685rl4mnk952945hzcn2dbidppv"; # Замініть на реальний хеш
+    rev = "d73842c761f7d7859f3bdd80e4360f09180fad41";
+    sha256 = "1lvbvs58w1jx2y490vb4vpwqs685rl4mnk952945hzcn2dbidppv";
   };
-
-  # Для цієї теми часто потрібні додаткові залежності (Qt6/Qt5 Quick components)
-  # Якщо ви використовуєте її в NixOS, переконайтеся, що ці пакунки є в sddm.extraPackages
-
   installPhase = ''
     mkdir -p $out/share/sddm/themes/sddm-astronaut-theme
     cp -R ./* $out/share/sddm/themes/sddm-astronaut-theme/
