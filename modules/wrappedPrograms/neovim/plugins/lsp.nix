91a943a55dab87de7f504d4448e4ac376a817de6
Archerymystery <84961834+Archerymystery@users.noreply.github.com>
2026-06-05 22:18:11 +0200

    feat(nvf): add better lsp for nix

--- modules/wrappedPrograms/neovim/plugins/lsp.nix
+++ modules/wrappedPrograms/neovim/plugins/lsp.nix
@@ -1,85 +1,110 @@
 {
   flake.neovimModules.lsp =
-    { ... }:
+    { lib, ... }:
     {
       vim = {
         lsp = {
           enable = true;
-          formatOnSave = true; 
-          lightbulb.enable = true; 
+          formatOnSave = true;
+          lightbulb.enable = true;
+          #   servers.clangd = {
+          #     cmd = lib.mkForce [
+          #       "clangd"
+          #       "--header-insertion=never"
+          #       "--background-index"
+          #     ];
+          #     initOptions = {
+          #       fallbackFlags = [ "-std=c++23" ];
+          #     };
+          #   };
+          servers.nixd = {
+            settings = {
+              nixd = {
+                nixpkgs = {
+                  expr = "import <nixpkgs> { }";
+                };
+                formatting = {
+                  command = [ "nixfmt" ];
+              };
+            };
+          };
         };
-
-        
+        autocmds = [
+          {
+            event = [ "BufWritePre" ];
+            pattern = [
+              "*.c"
+              "*.h"
+              "*.cpp"
+            ];
+            command = "lua vim.lsp.buf.format()";
+          }
+        ];
         languages = {
-          enableTreesitter = true; 
+          enableTreesitter = true;
           enableExtraDiagnostics = true;
 
-          
           clang = {
             enable = true;
             lsp.enable = true;
-            lsp.servers = ["clangd"]; 
-         
+            lsp.servers = [ "clangd" ];
+
           };
 
-          
           rust = {
             enable = true;
-            extensions.crates-nvim.enable = true; 
+            extensions.crates-nvim.enable = true;
             format = {
               enable = true;
-              type = ["rustfmt"];
+              type = [ "rustfmt" ];
             };
           };
 
-          
           python = {
             enable = true;
             format = {
               enable = true;
-              type = ["black"]; 
+              type = [ "black" ];
             };
             lsp.enable = true;
-            lsp.servers =["pyright"];
+            lsp.servers = [ "pyright" ];
           };
 
-          
           nix = {
             enable = true;
             format = {
               enable = true;
-              type = ["nixfmt"]; 
+              type = [ "nixfmt" ];
             };
-            lsp.enable = true;
-            lsp.servers = ["nil"]; 
+            lsp = {
+              enable = true;
+              servers = [ "nixd" ];
+            };
           };
-	  bash = {
-      enable = true;
-      lsp = {
-        enable = true;
-        servers = [ "bash-language-server" ];
-      };
-      format = {
-        enable = true;
-        type = ["shfmt"]; 
-      };
-    };
+          bash = {
+            enable = true;
+            lsp = {
+              enable = true;
+              servers = [ "bash-language-server" ];
+            };
+            format = {
+              enable = true;
+              type = [ "shfmt" ];
+            };
+          };
 
-    
-    cmake = {
-      enable = true;
-      lsp = {
-        enable = true;
-        servers = [ "neocmakelsp" ];
-      };
-    };
+          cmake = {
+            enable = true;
+            lsp = {
+              enable = true;
+              servers = [ "neocmakelsp" ];
+            };
+          };
 
-    
-    make = {
-      enable = true;
-      
-      
-    };
+          make = {
+            enable = true;
+
+          };
         };
       };
     };
